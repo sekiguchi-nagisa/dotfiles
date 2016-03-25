e96d59dde360df15425067fabe9d8c3122d84fa9
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2016-03-25 21:17:45 +0900

    replace RictyDiminished to Ricty

--- install-ricty-dim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo '--- copy fonts to /usr/share/fonts/truetype ---'
-sudo cp ./ext/RictyDiminished/*.ttf /usr/share/fonts/truetype
-
-echo '--- update font cache ---'
-sudo fc-cache -fv
