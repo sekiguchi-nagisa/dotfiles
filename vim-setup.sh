427d7e3af98d88f3fc0fe7c74a86a757a722452d
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2020-03-20 13:18:03 +0900

    remove old script

--- vim-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-SCRIPT_DIR="$(cd $(dirname $0) && pwd -P)"
-
-cp $SCRIPT_DIR/.vimrc ~/
-mkdir ~/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim \
-~/.vim/bundle/neobundle.vim
